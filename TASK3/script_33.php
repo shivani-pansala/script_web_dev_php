1d002a513fd8a36434418ea31dc880681f7b3358
shivani-pansala <143687310+shivani-pansala@users.noreply.github.com>
2023-10-26 16:57:21 +0530

    Add files via upload
    Create a PHP script to check if a specific key exists in an associative array using the
    array_key_exists() function.

---
+++ TASK3/script_33.php
@@ -0,0 +1,15 @@
+<?php
+
+$arrayis=array("India"=>"Rajkot","France"=>"Paris","England"=>"London");
+
+if(array_key_exists("France",$arrayis))
+  {
+    echo "Key is Exits";
+  }     
+  else
+  {
+    echo "Key is not Exits";
+  }
+
+
+?>
